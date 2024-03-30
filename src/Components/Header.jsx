f17c1a24de25f2e939a45658b3083248e5a88cbc
Amelss <ameleyka@gmail.com>
2024-03-30 21:59:09 +0000

    Adjusting header and removing dependencies no longer needed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,4 @@
-import Hamburger from "hamburger-react";
-import { useState } from "react";
-
 export default function Header() {
-  const [isOpen, setOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setOpen(!isOpen);
-  };
 
 
 
@@ -23,42 +15,7 @@ export default function Header() {
      };
   return (
     <div className="custom-font">
-      <div className="flex justify-between items-center">
-        <div className="xl:hidden">
-          <Hamburger toggled={isOpen} toggle={toggleMenu} />
-        </div>
-        <div className="px-3 text-lg xl:hidden">
-          <h1 className="text-blue-300 font-bold">Ameley In The Clouds</h1>
-        </div>
-      </div>
-      {isOpen && (
-        <div className=" bg-gray-100 h-screen opacity-90">
-          <ul className="list-none p-5">
-            <li className="py-2">
-              <a href="#about" className="link" onClick={handleSmoothScroll}>
-                About
-              </a>
-            </li>
-            <li className="py-2">
-              <a href="#experience" className="link" onClick={handleSmoothScroll}>
-                Experience
-              </a>
-            </li>
-            <li className="py-2">
-              <a href="#" className="link" onClick={handleSmoothScroll}>
-                Projects
-              </a>
-            </li>
-            <li className="py-2">
-              <a href="#" className="link" onClick={handleSmoothScroll}>
-                Skills
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
-
-      <div className="hidden xl:flex justify-between items-center py-3 px-5">
+      <div className="text-center text-sm xl:text-base xl:flex justify-between items-center py-3 px-5">
         <div>
           <h1 className="custom-font text-3xl font-bold text-blue-300">
             Ameley In The Clouds
