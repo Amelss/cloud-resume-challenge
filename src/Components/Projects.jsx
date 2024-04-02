2fa58d57c79491f241e18198eff7fcdafa35f4e8
Amelss <ameleyka@gmail.com>
2024-04-02 14:46:06 +0100

    updating projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
       <h1 className="text-center text-3xl font-bold text-blue-300 mb-5 pt-7 xl:text-4xl">
         Projects
       </h1>
-      <div className="grid grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-y-2  pb-7">
+      <div className="grid grid-cols-2 xl:grid-cols-4 3xl:grid-cols-4 gap-y-2  pb-7">
          {projectData}
     </div>
        
