327e70d7bb0f1a1d7b076ca475809833d383c24f
Amelss <ameleyka@gmail.com>
2024-04-11 16:48:14 +0100

    adding footer

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -4,6 +4,8 @@ import Experience from "./Experience"
 import Projects from "./Projects"
 import Skills from "./Skills"
 import Education from "./Education"
+import Footer from "./Footer"
+
 
 
 export default function Content() {
@@ -25,6 +27,9 @@ export default function Content() {
 
       <div>
         <Skills />
+      </div>
+      <div className="bg-gray-100 py-5">
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+  return (
+    <div className=" flex justify-center items-center">
+      <a href="https://github.com/Amelss" target="_blank" rel="noopener noreferrer"><img src="/assets/github.png" alt="github logo" className="w-10 h-10" /></a>
+     <a href="https://www.linkedin.com/in/ameley-kwei-armah-38a00310a/" target="_blank" rel="noopener noreferrer"><img src="/assets/linkedin.png" alt="linkedin logo" className="w-10 h-10 ml-3" /></a> 
+      <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fgenibi_bae/" target="_blank" rel="noopener noreferrer"><img src="/assets/twitter.png" alt="twitter logo" className="w-9 h-9 ml-3" /></a>
+    </div>
+  );
+}
